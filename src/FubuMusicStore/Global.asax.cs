5ef892fed29853ad013f686869a47f781d14dc96
Ryan Rauh <rauh.ryan@ninecollective.com>
2011-03-23 21:38:47 -0500

    removed the reference to NHProf. It broke on my machine

--- src/FubuMusicStore/Global.asax.cs
+++ src/FubuMusicStore/Global.asax.cs
@@ -21,7 +21,6 @@ namespace FubuMusicStore
                 .Packages(x => x.Assembly(typeof (IRepository).Assembly))
                 .Bootstrap(RouteTable.Routes);
 
-            HibernatingRhinos.Profiler.Appender.NHibernate.NHibernateProfiler.Initialize();
         }
 
         protected void Session_Start(object sender, EventArgs e)
